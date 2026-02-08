a7a47c060489a2906003b960f7d026cd233bd5ad
Daniel Queiroz <dqueirozcandido@gmail.com>
2026-02-07 23:57:03 -0300

    Função filtrar melhorada

--- to-do-list.py
+++ to-do-list.py
@@ -8,6 +8,7 @@
 
 
 tarefas = []
+tarefas_encontradas = []
 
 def inserir_tarefa():
     resposta4 = input("""
@@ -48,40 +49,45 @@ def deletar_tarefa():
     else:
         print("Escolha entre os numeros de 1 e 2!")
 
-def consultar_tarefas():
-    print("""
-            
-    Suas tarefas são: 
-    """)
-    task_num = 1
+# Filtragem adicional
+def buscar_tarefas():
+    nome_tarefa = input("Insira a tarefa que deseja encontrar: ")
+    tarefas_encontradas.clear()
+
+    inicial = False
     for task in tarefas:
+        if nome_tarefa in task:
+            inicial = True
+    if inicial == False:
+        print("Nenhuma tarefa correspondente")
+        home()
+
+    print("As tarefas correspondentes são: ")
+    for task in tarefas:
+        if nome_tarefa in task:
+            tarefas_encontradas.append(task)
+
+    task_num = 1
+    for task in tarefas_encontradas:
         print(f"{task_num} - {task}")
         task_num = task_num + 1
 
-    resposta2 = input("""
+    resposta8 = input("""
         Escolha dentre as opções:
         1 - Marcar tarefa
         2 - Voltar
         """)
-
-    print("""
-            
-    Suas tarefas atuais são: 
-    """)
-    task_num = 1
-    for task in tarefas:
-        print(f"{task_num} - {task}")
-        task_num = task_num + 1
-
-    if resposta2 == str(1):
+    
+    if resposta8 == str(1):
         tarefa_a_marcar = int(input("Número da tarefa que deseja marcar: "))
         numero_a_marcar = tarefa_a_marcar - 1
-        tarefas[numero_a_marcar] = tarefas[numero_a_marcar] + " (Completada)"
-    elif resposta2 == str(2):
+        nome_tarefa_puro = tarefas_encontradas[numero_a_marcar]
+        if nome_tarefa_puro in tarefas:
+            indice = tarefas.index(nome_tarefa_puro)
+        tarefas[indice] = tarefas[indice] + " (Completada)"
+
+    elif resposta8 == str(2):
         home()
-    else:
-        print("Escolha entre os numeros de 1 e 2!")
-        consultar_tarefas()
 
 def alterar_tarefa():
     resposta6 = int(input("""
@@ -109,19 +115,55 @@ def alterar_tarefa():
         home()
     else:
         print("Escolha entre os numeros de 1 e 2!")
+
+def consultar_tarefas():
+    print("""
+            
+    Suas tarefas são: 
+    """)
+    task_num = 1
+    for task in tarefas:
+        print(f"{task_num} - {task}")
+        task_num = task_num + 1
+
+    print("""
+        Escolha dentre as opções:
+        1 - Buscar por tarefa
+        2 - Marcar tarefa
+        3 - Voltar
+          
+        """)
+
+    resposta2 = int(input("Sua resposta: "))
+    print(resposta2)
+
+    print("""          
+    Suas tarefas atuais são: 
+    """)
+    task_num = 1
+    for task in tarefas:
+        print(f"{task_num} - {task}")
+        task_num = task_num + 1
     
+    if resposta2 == (1):
+        buscar_tarefas()
+    elif resposta2 == str(2):
+        tarefa_a_marcar = int(input("Número da tarefa que deseja marcar: "))
+        numero_a_marcar = tarefa_a_marcar - 1
+        tarefas[numero_a_marcar] = tarefas[numero_a_marcar] + " (Completada)"
+    elif resposta2 == str(3):
+        home()
 
 def home():
     rodando = True
-    while rodando:
+    while rodando == True:
         print("""
               
         Escolha dentre as opções:
         1 - Inserir tarefa
         2 - Deletar tarefa
-        3 - Consultar tarefas
-        4 - Alterar tarefa
-        5 - Sair
+        3 - Lista de tarefas
+        4 - Sair
         """)
 
         resposta = input("Sua escolha: ")
@@ -133,12 +175,10 @@ def home():
         elif resposta == str(3):
             consultar_tarefas()
         elif resposta == str(4):
-            alterar_tarefa()
-        elif resposta == str(5):
             print("Você Saiu!")
             rodando = False
         else:
-            print("Escolha entre os numeros de 1 a 5!")
+            print("Escolha entre os numeros de 1 a 4!")
             continue
 
 home()
